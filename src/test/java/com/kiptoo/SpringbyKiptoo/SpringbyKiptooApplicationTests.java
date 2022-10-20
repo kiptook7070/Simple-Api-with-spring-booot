42c3203f44c0f302c85791ab5a6dbc607d493c70
kiptook <Kiptook2020@gmail.com>
2022-10-20 10:20:07 +0300

    first commit

---
+++ src/test/java/com/kiptoo/SpringbyKiptoo/SpringbyKiptooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiptoo.SpringbyKiptoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbyKiptooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
